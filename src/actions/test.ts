fed448a791a946465471feb68fc798004087f384
Kdurand974 <k.durand.974@gmail.com>
2025-02-17 01:29:36 +0100

    add comment

--- src/actions/test.ts
+++ src/actions/test.ts
@@ -1,4 +1,6 @@
-// Use schema here
+"use server"
+
+// We can use schema here with no problem, but you CANT export other thing than server actions
 
 export const testFunction = () => "ok"
 export const testParamFunction = (param: string) => 500
